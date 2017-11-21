522af1d831fce0d48ab6af7dbc8195431371190d
Angélica <angelica.martinez16@utim.edu.mx>
2017-11-21 12:29:12 -0600

    cambio

--- src/sample/Cola.java
+++ src/sample/Cola.java
@@ -1,4 +1,13 @@
 package sample;
 
-public class Cola {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Cola implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
